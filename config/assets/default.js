768aec663846d55ae7ce5a6ffc3cd43686e39862
Hector Gonzalez <hector.gonzalez@enova.mx>
2015-09-22 17:55:55 -0500

    modal beta signup

--- config/assets/default.js
+++ config/assets/default.js
@@ -7,7 +7,8 @@ module.exports = {
         'public/lib/bootstrap/dist/css/bootstrap.css',
         'public/lib/bootstrap/dist/css/bootstrap-theme.css',
         'public/lib/bootstrap-social/bootstrap-social.css',
-        'public/lib/font-awesome/css/font-awesome.css'
+        'public/lib/font-awesome/css/font-awesome.css',
+        'public/santo.css'
       ],
       js: [
         'public/lib/angular/angular.js',
@@ -18,7 +19,9 @@ module.exports = {
         'public/lib/angular-ui-utils/ui-utils.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
         'public/lib/angular-file-upload/angular-file-upload.js',
-        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js'
+        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+        'public/lib/jquery/dist/jquery.min.js',
+        'public/lib/bootstrap/dist/js/bootstrap.min.js'
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
     },
